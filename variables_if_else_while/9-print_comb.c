dabeba3adbafea31964b9a98ab9c4c3c8049bb95
NkemJefersonAchia <nkemachia05@gmail.com>
2026-09-13 00:06:20 +0100

    9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit;
+
+	digit = 48;
+
+	while (digit <= 57)
+	{
+		putchar(digit);
+		if (digit != 57)
+		{
+			putchar(44);
+			putchar(' ');
+		}
+		digit++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
